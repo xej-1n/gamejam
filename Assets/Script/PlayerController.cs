47e73a2fbb581b916dcb7df9a8db2badbd52a37f
moha <djd468694@gmail.com>
2026-07-23 14:34:58 +0900

    주석제거

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -19,27 +19,23 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // 1. 애니메이터 파라미터 업데이트 (달리기만 남김)
         anim.SetBool("IsRunning", Mathf.Abs(moveInput.x) > 0.1f);
 
-        // 2. 캐릭터 좌우 반전 (Scale 사용)
         if (moveInput.x > 0)
         {
-            transform.localScale = new Vector3(2.5f, 2.5f, 1); // 오른쪽
+            transform.localScale = new Vector3(2.5f, 2.5f, 1);
         }
         else if (moveInput.x < 0)
         {
-            transform.localScale = new Vector3(-2.5f, 2.5f, 1); // 왼쪽 (X축 반전)
+            transform.localScale = new Vector3(-2.5f, 2.5f, 1);
         }
     }
 
     void FixedUpdate()
     {
-        // 3. 물리 이동
         rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
     }
 
-    // --- 인풋 시스템 입력 ---
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
